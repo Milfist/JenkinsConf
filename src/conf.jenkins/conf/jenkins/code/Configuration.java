fbd5ea11003e617ec7628071cc139df76b530152
MilfisT <milfist@gmail.com>
2018-05-10 11:58:43 +0200

    [CHORE] Configuring the project

---
+++ src/conf.jenkins/conf/jenkins/code/Configuration.java
@@ -0,0 +1,9 @@
+package conf.jenkins.code;
+
+public class Configuration {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!!!!!!");
+    }
+
+}
---
+++ src/conf.jenkins/module-info.java
@@ -0,0 +1,3 @@
+module conf.jenkins {
+
+}
