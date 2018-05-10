dfbe8b48b12781313da3ab88af79e9cb5795e30d
MilfisT <milfist@gmail.com>
2018-05-10 12:44:02 +0200

    [CHORE] cause an error

--- src/conf.jenkins/conf/jenkins/code/Configuration.java
+++ src/conf.jenkins/conf/jenkins/code/Configuration.java
@@ -3,7 +3,7 @@ package conf.jenkins.code;
 public class Configuration {
 
     public static void main(String[] args) {
-        System.out.print("Hello world!!!!!!");
+        System.out.printy("Hello world!!!!!!");
     }
 
 }
